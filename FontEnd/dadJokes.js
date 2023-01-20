d02c386564a019b88ac0896f301080f773e8e225
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2023-01-20 15:10:46 -0500

    Update dadJokes.js

--- FontEnd/dadJokes.js
+++ FontEnd/dadJokes.js
@@ -42,7 +42,78 @@ async function loadData(url){
         <td>${setup}</td>
         <td>${punch}</td>
         </tr>`
+    });const api_url = "https://localhost:7134/api/Jokes"
+const random_URL ="https://localhost:7134/api/Jokes/random"
+
+async function loadData(url){
+
+    
+    const tablebody = document.querySelector('#tbody');
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    
+
+    data.forEach(jokeObj => {
+        let{id,category,setup,punch} = jokeObj;
+        tablebody.innerHTML += `<tr>
+        <td>${id}</td>
+        <td>${category}</td>
+        <td>${setup}</td>
+        <td>${punch}</td>
+        </tr>`
     });
+    
+}
+
+loadData(api_url, document.querySelector("table"));
+
+async function loadRandom(url){
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    
+    document.getElementById("randomJoke").innerHTML= `${data.setup} <div></div> ${data.punch}`    
+}
+
+loadRandom(random_URL);
+
+async function type(url){
+    
+    const responseType = await fetch(url);
+    const types = await responseType.json();
+
+    const selectBox = document.querySelector('#select_type')
+    types.forEach(t =>
+        {
+            var opt =document.createElement('option');
+            opt.value=t;
+            opt.innerHTML=t;
+            selectBox.appendChild(opt); 
+        });   
+        
+}
+
+type("https://localhost:7134/api/Jokes/types");
+
+
+async function jokeByType (){
+
+    
+    document.getElementById("search_type").onclick = async () =>{
+        let x = document.getElementById("select_type");
+        let type = x.value;  
+        
+        const response = await fetch(`https://localhost:7134/api/Jokes/bytype?type=${type}`);
+        const joke = await response.json();
+
+        document.getElementById("joke-content").innerHTML= `${joke.setup} <div></div> ${joke.punch}`;        
+    }
+
+    
+}
+jokeByType();
     
 }
 
