d50e24b8dd00a3f65a6af26f9811eb669944ece5
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2023-01-20 09:42:40 -0500

    Update dadJokes.js

--- FontEnd/dadJokes.js
+++ FontEnd/dadJokes.js
@@ -22,3 +22,39 @@ async function loadData(url){
 }
 
 loadData(api_url, document.querySelector("table"));
+const api_url = "https://localhost:7134/api/Jokes"
+const random_URL ="https://localhost:7134/api/Jokes/random"
+
+async function loadData(url){
+
+    
+    const tablebody = document.querySelector('#tbody');
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    
+
+    data.forEach(jokeObj => {
+        let{id,category,setup,punch} = jokeObj;
+        tablebody.innerHTML += `<tr>
+        <td>${id}</td>
+        <td>${category}</td>
+        <td>${setup}</td>
+        <td>${punch}</td>
+        </tr>`
+    });
+    
+}
+
+loadData(api_url, document.querySelector("table"));
+
+async function loadRandom(url){
+    
+    const response = await fetch(url);
+    const data = await response.json();
+    
+    
+    document.getElementById("randomJoke").innerHTML= `${data.setup} <div></div> ${data.punch}`    
+}
+
+loadRandom(random_URL);
