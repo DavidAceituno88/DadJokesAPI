300f17a89fc298a1b98066fe5498a309f0239c3e
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2023-01-30 19:11:09 -0500

    Update BasicAuthenticationHandler.cs

--- Handler/BasicAuthenticationHandler.cs
+++ Handler/BasicAuthenticationHandler.cs
@@ -20,3 +20,14 @@ namespace DadJokesAPI.Handler
         }
     }
 }
+using Microsoft.EntityFrameworkCore;
+
+namespace DadJokesAPI.Models
+{
+    public class JokeDbContext : DbContext 
+    {
+        public JokeDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<Joke> Jokes { get; set; }
+        public DbSet<Users> User { get; set; }
+    }
+}
